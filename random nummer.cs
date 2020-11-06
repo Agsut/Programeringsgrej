fe02e0695ac5ae812af167ad350a9ee394475757
Agsut <70062634+Agsut@users.noreply.github.com>
2020-11-06 09:42:59 +0100

    Add files via upload
    https://docs.google.com/document/d/1USBLxPNN4v-GQKnphrTSlQ7xbJ6vQCkmjC85nrwgMMw/edit

---
+++ random nummer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+
+            int rand_num = rd.Next(1, 100);
+
+            Console.ReadLine();
+        }
+    }
+}
